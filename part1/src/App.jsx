813c4763f98b0c262049d9511542124c423c02e3
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-02-23 20:29:29 +0545

    Passing state to child component to use separate Display and Button components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,43 +1,30 @@
 import {useState} from 'react'
 
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  // const name = 'Peter'
-  // const age = 10
-
-  // return (
-  //   <div>
-  //     <h1>Greetings</h1>
-  //     <Hello name="Maya" age={26 + 10} />
-  //     <Hello name={name} age={age} />
-  //   </div>
-  // )
-  
   const [counter, setCounter] = useState(0)
 
-  // const handleClick = () => {
-  //   console.log('clicked')
-  //   setCounter(counter + 1)
-  // }
+  const Display = (props) => {
+    return (
+      <div>{props.counter}</div>
+    )
+  }
+  
+  const Button = (props) => {
+    return (
+      <button onClick={props.onClick}>
+        {props.text}
+      </button>
+    )
+  }
 
   console.log('rendering...', counter)
 
   return (
     <>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter+1)}>plus</button>
-      <button onClick={() => setCounter(0)}>zero</button>
+      <Display counter={counter} />
+      <Button onClick={() => setCounter(counter+1)} text='plus' />
+      <Button onClick={() => setCounter(0)} text='zero' />
+      <Button onClick={() => setCounter(counter-1)} text='minus' />
     </>
   )
 }
