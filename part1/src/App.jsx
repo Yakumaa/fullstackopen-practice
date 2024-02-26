73f48ab8397e33a34781403d66298a1e4a552e59
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-02-26 10:09:48 +0545

    Add total count of clicks: asynchronous update of state

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,14 +5,17 @@ const App = () => {
     left: 0, right: 0
   })
   const [allClicks, setAllClicks] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAllClicks(allClicks.concat('L'))
     setClicks({ ...clicks, left: clicks.left + 1 })
+    setTotal(clicks.left + clicks.right + 1)
   }
   const handleRightClick = () => {
     setAllClicks(allClicks.concat('R'))
     setClicks({ ...clicks, right: clicks.right + 1 })
+    setTotal(clicks.left + clicks.right + 1)
   }
   return (
     <div>
@@ -21,6 +24,7 @@ const App = () => {
       <button onClick={handleRightClick}>right</button>
       {clicks.right}
       <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
