7d9d40132b626a672d38e92f5831b70fd8760f03
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-05-06 15:48:47 +0545

    Refactor file paths and update import statements

--- part3/tests/note_api.test.js
+++ part3/tests/note_api.test.js
@@ -1,21 +1,43 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    important: true,
+  }
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+test.only('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are two notes', async () => {
+test.only('there are two notes', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('the first note is about HTTP methods', async () => {
