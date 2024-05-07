8d942c8156daa3e18fd3e483d210792f980b264d
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-05-07 13:56:24 +0545

    Refactor GET route in notes.js to use async/await and update response status

--- part3/tests/note_api.test.js
+++ part3/tests/note_api.test.js
@@ -47,6 +47,19 @@ test('the first note is about HTTP methods', async () => {
   assert(contents.includes('HTML is easy'))
 })
 
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultNote.body, noteToView)
+})
+
 test('a valid note can be added ', async () => {
   const newNote = {
     content: 'async/await simplifies making async calls',
@@ -79,6 +92,22 @@ test('note without content is not added', async () => {
   const notesAtEnd = await helper.notesInDb()
 
   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  const contents = notesAtEnd.map(r => r.content)
+  assert(!contents.includes(noteToDelete.content))
+  
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
 })
 
 after(async () => {
