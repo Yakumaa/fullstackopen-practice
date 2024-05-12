72ab0d5dfc3e93fc72cfacc2e889d9834a40c7d5
Yakumaa <maharjanshrish8@gmail.com>
2024-05-12 21:46:16 +0545

    limiting creating new notes to logged-in users

--- part3/utils/middleware.js
+++ part3/utils/middleware.js
@@ -21,8 +21,14 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if(error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')){
     return response.status(400).json({ error: 'expected `username` to be unique' })
+  } else if(error.name === 'JsonWebTokenError'){
+    return response.status(401).json({ error: 'invalid token' })
+  } else if(error.name === 'ToeknExpiredError'){
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
-
+  
   next(error)
 }
 
