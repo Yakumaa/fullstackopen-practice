3752a81ead1a34eee6a2280f0af5979041f61f66
Yakumaa <maharjanshrish8@gmail.com>
2024-07-18 22:18:14 +0545

    chore: Update .gitignore and .eslintrc.cjs, add LoginForm, NoteForm, Togglable components and Used useRef hook

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,8 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
 import loginService from './services/login'
+import LoginForm from './components/LoginForm'
+import NoteForm from './components/NoteForm'
+import Togglable from './components/Togglable'
 
 const Footer = () => {
   const footerStyle = {
@@ -23,6 +26,7 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [loginVisible, setLoginVisible] = useState(false)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -45,17 +49,8 @@ const App = () => {
     }
   }, [])
 
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      // id: notes.length + 1,
-    }
-    // setNotes(notes.concat(noteObject))
-    // setNewNote('')
-
+  const addNote = (noteObject) => {
+    noteFormRef.current.toggleVisibility()
     noteService
     .create(noteObject)
     .then(returnedNote => {
@@ -84,10 +79,10 @@ const App = () => {
       })
   }
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+  // const handleNoteChange = (event) => {
+  //   console.log(event.target.value)
+  //   setNewNote(event.target.value)
+  // }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -116,38 +111,35 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type='text'
-            value={username}
-            name='Username'
-            onChange={({ target}) => setUsername(target.value)}
-          />
+  const loginForm = () => {
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    const showWhenVisible = { display: loginVisible ? '' : 'none' }
+
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>login</button>
         </div>
-        <div>
-          password
-          <input
-            type='password'
-            value={password}
-            name='Password'
-            onChange={({ target}) => setPassword(target.value)}
+        <div style={showWhenVisible}>
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
           />
+          <button onClick={() => setLoginVisible(false)}>cancel</button>
         </div>
-        <button type='submit'>login</button>
-      </form>
-  )
+      </div>
+    )
+  }
+
+  const noteFormRef = useRef()
 
   const noteForm = () => (
-    <form onSubmit={addNote}>
-      <input
-        value = {newNote}
-        onChange = {handleNoteChange}
-      />
-      <button type='submit'>save</button>
-    </form>
+    <Togglable buttonLabel='new note' ref={noteFormRef}>
+      <NoteForm createNote={addNote} />
+    </Togglable>
   )
 
   return (
@@ -160,6 +152,9 @@ const App = () => {
         <div>
           <p>{user.name} logged-in</p>
           {noteForm()}
+          {/* <Togglable buttonLabel='new note'>
+            <NoteForm createNote={addNote} />
+          </Togglable> */}
         </div>
       }
       
