fab78d001364a81a0b7721ce35a4274dd1eaf458
Yakumaa <maharjanshrish8@gmail.com>
2024-08-06 16:59:22 +0545

    testing react apps: Update .gitignore, vite.config.js and add testSetup.js

--- part2/src/components/NoteForm.jsx
+++ part2/src/components/NoteForm.jsx
@@ -1,30 +1,31 @@
 import { useState } from 'react'
 const NoteForm = ({ createNote }) => {
+	const [newNote, setNewNote] = useState('')
 
-  const [newNote, setNewNote] = useState('')
+	const addNote = (event) => {
+		event.preventDefault()
+		createNote({
+			content: newNote,
+			important: false,
+		})
 
-  const addNote = (event) => {
-    event.preventDefault()
-    createNote({
-      content: newNote,
-      important: false,
-    })
+		setNewNote('')
+	}
 
-    setNewNote('')
-  }
+	return (
+		<div>
+			<h2>Create a new note</h2>
 
-  return (
-    <div>
-      <h2>Create a new note</h2>
-
-      <form onSubmit={addNote}>
-        <input 
-          value={newNote} 
-          onChange={event => setNewNote(event.target.value)} />
-        <button type="submit">save</button>
-      </form>
-    </div>
-  )
+			<form onSubmit={addNote}>
+				<input
+					value={newNote}
+					onChange={(event) => setNewNote(event.target.value)}
+					placeholder="write note content here"
+				/>
+				<button type="submit">save</button>
+			</form>
+		</div>
+	)
 }
 
 export default NoteForm
