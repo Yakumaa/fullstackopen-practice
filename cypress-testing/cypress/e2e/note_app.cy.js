e25a6c5f2d2b94f620885a28dc75a03fa7b52001
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-08-15 08:55:47 -0700

    feat: Add login functionality to Note app

--- cypress-testing/cypress/e2e/note_app.cy.js
+++ cypress-testing/cypress/e2e/note_app.cy.js
@@ -1,7 +1,19 @@
 describe('Note app', function () {
-	it('front page can be opened', function () {
+	beforeEach(function () {
 		cy.visit('http://localhost:5173')
+	})
+
+	it('front page can be opened', function () {
 		cy.contains('Notes')
 		cy.contains('HTML is easy')
+	})
+
+	it('login form can be opened', function () {
+		cy.contains('login').click()
+		cy.get('#username').type('admin')
+		cy.get('#password').type('1234')
+		cy.get('#login-button').click()
+
+		cy.contains('shrish logged-in')
 	})
 })
--- part2/src/components/LoginForm.jsx
+++ part2/src/components/LoginForm.jsx
@@ -1,36 +1,22 @@
-const LoginForm = ({
-  handleSubmit,
-  handleUsernameChange,
-  handlePasswordChange,
-  username,
-  password
-}) => {
-  return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          username
-          <input
-            type='text'
-            value={username}
-            name='Username'
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type='password'
-            value={password}
-            name='Password'
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type='submit'>login</button>
-      </form>
-    </div>
-  )
+const LoginForm = ({ handleSubmit, handleUsernameChange, handlePasswordChange, username, password }) => {
+	return (
+		<div>
+			<h2>Login</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					username
+					<input type="text" id="username" value={username} name="Username" onChange={handleUsernameChange} />
+				</div>
+				<div>
+					password
+					<input type="password" id="password" value={password} name="Password" onChange={handlePasswordChange} />
+				</div>
+				<button id="login-button" type="submit">
+					login
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default LoginForm
