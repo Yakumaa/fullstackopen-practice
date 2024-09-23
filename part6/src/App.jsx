6a9425ad82edbf42a69a1b370222603adb873fe2
Yakumaa <maharjanshrish8@gmail.com>
2024-09-23 14:44:29 +0545

    uncontrolled form and forwarding redux store to various components

--- part6/src/App.jsx
+++ part6/src/App.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
-
-import { createStore } from 'redux'
-import reducer from './reducers/noteReducer'
+import { createNote, toggleImportanceOf } from './reducers/noteReducer'
+import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-	const store = createStore(reducer)
+	const dispatch = useDispatch()
+	const notes = useSelector((state) => state)
 
-	store.dispatch({
-		type: 'NEW_NOTE',
-		payload: {
-			content: 'the app state is in redux store',
-			important: true,
-			id: 1,
-		},
-	})
+	const addNote = (event) => {
+		event.preventDefault()
+		const content = event.target.note.value
+		event.target.note.value = ''
 
-	store.dispatch({
-		type: 'NEW_NOTE',
-		payload: {
-			content: 'state changes are made with actions',
-			important: false,
-			id: 2,
-		},
-	})
+		dispatch(createNote(content))
+	}
+
+	const toggleImportance = (id) => {
+		dispatch(toggleImportanceOf(id))
+	}
+
 	return (
 		<div>
+			<form onSubmit={addNote}>
+				<input name="note" />
+				<button type="submit">add</button>
+			</form>
 			<ul>
-				{store.getState().map((note) => (
-					<li key={note.id}>
+				{notes.map((note) => (
+					<li key={note.id} onClick={() => toggleImportance(note.id)}>
 						{note.content} <strong>{note.important ? 'important' : ''}</strong>
 					</li>
 				))}
--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
+import noteReducer from './reducers/noteReducer'
 
+const store = createStore(noteReducer)
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
+	<Provider store={store}>
 		<App />
-	</StrictMode>
+	</Provider>
 )
