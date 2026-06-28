32048a0f7bcdc8d8b23af49d396a4ddaf5b2cfee
Yakumaa <maharjanshrish8@gmail.com>
2026-06-28 09:00:11 +0545

    feat: implement visibility filter for notes and update main app structure

--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -1,10 +1,19 @@
 import { createRoot } from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
+import filterReducer from './reducers/filterReducer'
 import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+})
+
+const store = createStore(reducer)
+
+console.log(store.getState())
+
 createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
 		<App />
