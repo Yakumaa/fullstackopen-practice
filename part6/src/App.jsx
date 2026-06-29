44243bc50d4cca9770b5535dac9bbf3e87b3da7e
Yakumaa <maharjanshrish8@gmail.com>
2026-06-29 19:13:39 +0545

    feat: integrate json-server for notes management and update note handling

--- part6/src/App.jsx
+++ part6/src/App.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import Notes from './components/Notes'
 import NewNote from './components/NewNote'
 import VisibilityFilter from './components/VisibilityFilter'
+import { setNotes } from './reducers/noteReducer'
+import noteService from './services/notes'
 
 const App = () => {
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    noteService.getAll().then(notes => dispatch(setNotes(notes)))
+  }, [dispatch])
+  
 	return (
 		<div>
 			<NewNote />
